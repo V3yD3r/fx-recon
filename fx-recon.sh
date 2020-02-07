a5f757798c16a1b70a3edc739ba07c433e8c7e2e
V3yD3r <59905485+V3yD3r@users.noreply.github.com>
2020-02-06 19:59:18 -0500

    Update fx-recon.sh

--- fx-recon.sh
+++ fx-recon.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-~/h4k/scripts/fx-recon/recon-prestart.sh
-~/h4k/scripts/fx-recon/recon-subdomain-enum.sh $1
+~/tools/fx-recon/recon-prestart.sh
+~/tools/fx-recon/recon-subdomain-enum.sh $1
 
-~/h4k/scripts/fx-recon/recon-dirsearch-domainlist.sh ./*-domains
-~/h4k/scripts/fx-recon/recon-dirsearch-domainlist.sh ./results/all-domains-sub
+~/tools/fx-recon/recon-dirsearch-domainlist.sh ./*-domains
+~/tools/fx-recon/recon-dirsearch-domainlist.sh ./results/all-domains-sub
 
-~/h4k/scripts/fx-recon/recon-httprobe.sh ./results/all-domains-sub
-~/h4k/scripts/fx-recon/recon-httprobe.sh ./results/all-domains-dirsearched
+~/tools/fx-recon/recon-httprobe.sh ./results/all-domains-sub
+~/tools/fx-recon/recon-httprobe.sh ./results/all-domains-dirsearched
 
+~/tools/fx-recon/recon-differ.sh
 
-~/h4k/scripts/fx-recon/recon-differ.sh
-
-
-~/h4k/scripts/fx-recon/recon-nmapper.sh ./results/all-domains-sub
+~/tools/fx-recon/recon-nmapper.sh ./results/all-domains-sub
